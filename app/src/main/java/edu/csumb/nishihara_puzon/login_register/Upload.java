eb619798b1e4ce2235eb5a102236a4826b93abea
Nick Ishihara <nicholes86@gmail.com>
2016-04-13 19:18:42 -0700

    adding in upload

---
+++ app/src/main/java/edu/csumb/nishihara_puzon/login_register/Upload.java
@@ -0,0 +1,15 @@
+package edu.csumb.nishihara_puzon.login_register;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+/**
+ * Created by Nick on 4/13/16.
+ */
+public class Upload extends Activity implements View.OnClickListener {
+
+}
